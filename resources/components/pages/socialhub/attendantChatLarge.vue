7f6468be5ac7c2384dfcc310e5c7c04368febb3e
Your Name <you@example.com>
2019-12-11 20:20:52 -0300

    Alberto Local

--- resources/components/pages/socialhub/attendantChatLarge.vue
+++ resources/components/pages/socialhub/attendantChatLarge.vue
@@ -1199,7 +1199,7 @@
                 wsHost: process.env.MIX_APP_HOST,
                 // wsHost: window.location.hostname,
                 wsPort: 6001,
-                wssPort: 6001,
+                // wssPort: 6001,
                 enabledTransports: ['ws'],
                 // enabledTransports: ['ws', 'wss'],
                 // encrypted: true,
