a0b882551c2a529cc8f5ee82618c5f8e908e5ea9
Your Name <you@example.com>
2019-12-11 16:49:09 -0300

    Alberto Local

--- app/Http/Controllers/ExtendedRpiController.php
+++ app/Http/Controllers/ExtendedRpiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Repositories\ExtendedRpiRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Prettus\Repository\Criteria\RequestCriteria;
 
 class ExtendedRpiController extends RpiController
 {
@@ -21,16 +20,11 @@ class ExtendedRpiController extends RpiController
     public function index(Request $request)
     {
         $User = Auth::check() ? Auth::user() : session('logged_user');
+        $rpis = $this->rpiRepository->rpiOfCompany((int) $User->company_id); 
         if ($User->role_id == ExtendedContactsStatusController::MANAGER) {
-            $rpis = $this->rpiRepository->rpiOfCompany((int) $User->company_id); 
-            
             $QRCode = ExternalRPIController::getQRCode($rpis);
 
             $rpis->QRCode = $QRCode;
-        }else{            
-            // $this->rpiRepository->pushCriteria(new RequestCriteria($request));
-            // $rpis = $this->rpiRepository->all();
-            $rpis = null;
         }
         return $rpis->toJson();
     }
