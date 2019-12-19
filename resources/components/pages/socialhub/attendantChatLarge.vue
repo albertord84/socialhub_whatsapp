9740f35edc2a10cb10f8787e00d4926c88c313d6
josergm86 <josergm86@gmail.com>
2019-12-19 12:30:14 -0300

    a

--- resources/components/pages/socialhub/attendantChatLarge.vue
+++ resources/components/pages/socialhub/attendantChatLarge.vue
@@ -592,11 +592,9 @@
         </b-modal>
 
         <!-- Modal to show image-->
-        <b-modal v-model="modalShowImage" :hide-footer="true" :hide-header="true" size="lg" style="background-color:transparent !important" class="m-0 p-0 modal-body-bg">
-            <div >
-                <img class="img-fluid" style="max-height:600px; max-width:400px; " :src="modalShowImageSrc"/>
+        <b-modal v-model="modalShowImage" :hide-footer="true" :hide-header="true" size="lg" style="background-color:red !important; padding:0px; " class="">
+                <img class="img-fluid" style="max-height:540px; max-width:400px; " :src="modalShowImageSrc"/>
                 <!-- <img style="width:100%; height:100%" class="embed-responsive-item modal-body-bg" :src="modalShowImageSrc"/> -->
-            </div>
         </b-modal>
 
         <!-- Modal to show video-->
