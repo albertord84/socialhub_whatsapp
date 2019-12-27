10373b03d78bf8af1ed413c2df6c9d3d37065bf6
Your Name <you@example.com>
2019-12-27 18:27:40 -0300

    Alberto Local

---
+++ database/migrations/2019_12_27_181938_add_unique_key_contact_company_to_contact_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUniqueKeyContactCompanyToContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->unique( array('company_id', 'whatsapp_id') );
+        });
+    }
+
+}
