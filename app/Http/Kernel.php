4a1677f4a95828971dabe45938cb1384bca178ef
josergm86 <josergm86@gmail.com>
2020-01-02 11:09:45 -0300

    after resolve conflicts

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
 
-        \Barryvdh\Cors\HandleCors::class, // ECR
+        // \Barryvdh\Cors\HandleCors::class, // ECR
 
     ];
 
