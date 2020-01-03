395a56c363ae70a34da335b9877bc1ea61ccab94
Your Name <you@example.com>
2020-01-03 16:56:23 -0300

    Auto message

--- app/Http/Controllers/ExternalRPIController.php
+++ app/Http/Controllers/ExternalRPIController.php
@@ -328,7 +328,7 @@ class ExternalRPIController extends Controller
         $Chat->type_id = $type_id;
         $Chat->status_id = MessagesStatusController::UNREADED;
         $Attendnat = isset($AttendantsContact->attendant_id) ? UsersAttendant::find($AttendantsContact->attendant_id) : null;
-        if ($Attendnat && $Attendnat->selected_contact_id) {
+        if ($Attendnat && $Contact && $Attendnat->selected_contact_id == $Contact->id) {
             $Chat->status_id = MessagesStatusController::READED;
         }
         $Chat->socialnetwork_id = 1; // WhatsApp
