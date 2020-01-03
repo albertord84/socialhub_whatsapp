aa52eefd16892fbb6d5bee64358f572c7106d121
egbertocr <egberto.caballero@gmail.com>
2020-01-03 08:58:05 -0300

    atualizando codigo

--- app/Http/Controllers/ExtendedUsersSellerController.php
+++ app/Http/Controllers/ExtendedUsersSellerController.php
@@ -108,21 +108,10 @@ class ExtendedUsersSellerController extends UsersSellerController
 
     public function cep($cep){
         try{
-            $datas = file_get_contents('https://viacep.com.br/ws/'.$cep.'/json/');
-            // if(!strpos($datas,'erro')>0){
-                return $datas; //ja esta em json
-            // }else{
-            //     $obj = new stdClass();
-            //     $obj->message = "CEP not found";
-            //     // dd($obj->message);
-            //     return $obj->message->toJson();
-            // }
+            $datas = file_get_contents('https://viacep1.com.br/ws/'.$cep.'/json/');
+            return $datas; //ja esta em json
         } catch (\Throwable $th) {
-            $obj = new stdClass();
-            $obj->message = "An error occurr";
-            // dd($obj->message);
-            return $obj->toJson();
-            // throw $th;
+            return response()->json(['error' => 'Erro validando CEP'], 401);
         }
     }
     // public function cep($cep){
