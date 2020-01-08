a828627e878b16edb727f1940494ee6643ec10cd
Alberto <admin@socialhub.pro>
2020-01-08 09:22:24 -0300

    Auto message

--- tests/Unit/App/Controllers/Extended/ExtendedUsersAttendantControllerTest.php
+++ tests/Unit/App/Controllers/Extended/ExtendedUsersAttendantControllerTest.php
@@ -59,4 +59,30 @@ class ExtendedUsersAttendantControllerTest extends MyTestCase
         $this->assertCount(2, $attendants);
     }
 
+    public function testRoutePostUsersAttendantReturnsCreatedUser()
+    {
+        // Login Manager
+        $manager_id = 3;
+        $Manager = User::find($manager_id);
+        Auth::login($Manager);
+
+        // Create Mock User Into DB Test
+        $User = new UsersAttendant();
+        // Delete Mocked
+        $User->delete();
+
+        // Insert Mocked throw /users Post
+        $response = $this->be($Manager)->post('/users', $User->toArray());
+
+        $responseContent = $response->getContent();
+        $responseUser = json_decode($responseContent);
+
+        $response->assertSuccessful();
+        $this->assertJson($responseContent);
+        $this->assertEquals($User->id+1, $responseUser->id); // Expected to be the next to be inserted  
+        $this->assertEquals($User->email, $responseUser->email);
+        $this->assertEquals($User->password, $responseUser->password);
+        $this->assertEquals($User->company_id, $responseUser->company_id);
+
+    }    
 }
