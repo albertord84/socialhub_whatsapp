6827057c40641f81f861a42760a93080e538035e
josergm86 <josergm86@gmail.com>
2020-02-01 16:21:03 -0300

    a

--- resources/menu_admin.js
+++ resources/menu_admin.js
@@ -3,11 +3,7 @@ const menu_items = [
         name: 'Sellers',
         link: '/admin',
         icon: 'fa fa-address-book-o'
-    }, {
-        name: 'Testar frondend',
-        link: '/frontendtest',
-        icon: ' fa fa-home'
-    },
+    }, 
 
 ];
 export default menu_items;
