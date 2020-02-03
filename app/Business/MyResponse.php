853b4645ca3ba1ecff742f82bc551bd421b114d6
Alberto <admin@socialhub.pro>
2020-02-03 18:39:07 -0300

    Auto message

--- app/Business/MyResponse.php
+++ app/Business/MyResponse.php
@@ -51,6 +51,8 @@ class MyResponse
     }
 
     public static function makeExceptionJson(Throwable $th) {
+        throw $th;
+        
         Log::error('makeExceptionJson Throwable: ', [$th]);
         return self::makeResponseJson($th->getMessage(), $th->getTrace(), $th->getCode(), false);
     }
