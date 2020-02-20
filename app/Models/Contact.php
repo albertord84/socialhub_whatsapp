0e44bbbc28e91c83acc873938dc29532067b4427
egbertocr <egberto.caballero@gmail.com>
2020-02-20 12:40:21 -0300

    aa

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -53,6 +53,12 @@ class Contact extends Model
         'linkedin_id',
         'json_data',
         'status_id',
+
+        'cidade',
+        'estado',
+        'categoria1',
+        'categoria2',
+
     ];
 
     /**
@@ -77,6 +83,11 @@ class Contact extends Model
         'json_data' => 'string',
         'status_id' => 'integer',
         'updated_at' => 'string',
+        
+        'cidade' => 'string',
+        'estado' => 'string',
+        'categoria1' => 'string',
+        'categoria2' => 'string',
     ];
 
     /**
