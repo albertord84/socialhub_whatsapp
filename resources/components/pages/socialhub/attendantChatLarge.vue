165bc9d3a9691755a199b381e5837eaf4de799dd
josergm86 <josergm86@gmail.com>
2020-02-20 10:57:51 -0300

    download file

--- resources/components/pages/socialhub/attendantChatLarge.vue
+++ resources/components/pages/socialhub/attendantChatLarge.vue
@@ -1998,7 +1998,7 @@
                     {'responseType': 'blob'}, // important
                     )
                     .then((response)=>{
-                        const url = window.URL.createObjectURL(new Blob([response.data]));
+                        const url = window.URL.createObjectURL(new Blob([response.data], { type: 'application/'+ClientOriginalExtension }));
                         const link = document.createElement('a');
                         link.href = url;
                         link.setAttribute('download', ClientOriginalName); //or any other extension
