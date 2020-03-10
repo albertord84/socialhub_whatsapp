c86ac20ed696061da5cca540746f2fd51abd3747
Your Name <you@example.com>
2020-03-10 18:56:37 -0300

    Auto message

--- resources/components/pages/socialhub/attendantChatLarge.vue
+++ resources/components/pages/socialhub/attendantChatLarge.vue
@@ -2070,16 +2070,16 @@
             this.$store.commit('leftside_bar', "close");
             this.$store.commit('rightside_bar', "close");
             
-            if(this.handleTimeToReloadContacts){
-                clearInterval(this.handleTimeToReloadContacts);
-            }
-            if(process.env.MIX_TIME_TO_RELOAD_CONTACS){
-                this.handleTimeToReloadContacts = setInterval(()=>{
-                    console.log("Reloading all chats by time");
-                    this.getContacts();
-                    this.getAmountContactsInBag();                    
-                }, process.env.MIX_TIME_TO_RELOAD_CONTACS*1000);
-            }
+            // if(this.handleTimeToReloadContacts){
+            //     clearInterval(this.handleTimeToReloadContacts);
+            // }
+            // if(process.env.MIX_TIME_TO_RELOAD_CONTACS){
+            //     this.handleTimeToReloadContacts = setInterval(()=>{
+            //         console.log("Reloading all chats by time");
+            //         this.getContacts();
+            //         this.getAmountContactsInBag();                    
+            //     }, process.env.MIX_TIME_TO_RELOAD_CONTACS*1000);
+            // }
             
 
         },
