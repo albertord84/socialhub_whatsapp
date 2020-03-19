5b22d2e890a3f1452af5e00c90d294b671a84e57
Your Name <you@example.com>
2020-03-19 13:31:30 -0300

    Auto message

--- app/Jobs/SendWhatsAppMsg.php
+++ app/Jobs/SendWhatsAppMsg.php
@@ -9,18 +9,30 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
 
 class SendWhatsAppMsg implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $tries = 3;
+    
+    
+    /**
+     * O --timeoutvalor sempre deve ser pelo menos alguns segundos menor que o retry_aftervalor da configuração. 
+     *Isso garantirá que um trabalhador que processa um determinado trabalho seja sempre morto antes que o trabalho seja tentado novamente. 
+     *Se sua --timeoutopção for maior que o retry_aftervalor de configuração, seus trabalhos poderão ser processados ​​duas vezes.     
+     */
+    public $timeout = 25;
+    // Time worker will wait to retry again each work
+    public $retryAfter = 30;
+    
+    // Time worker will be sleeping wheter not work
+    public $sleep = 3;
 
-    public $retryAfter = 10;
-
-    // public $delay = 10;
     public $deleteWhenMissingModels = true;
 
+    
     private $rpiController;
 
     private $Contact;
@@ -44,6 +56,12 @@ class SendWhatsAppMsg implements ShouldQueue
         $this->file_content = $file_content;
         $this->file_name = $file_name;
         $this->file_type = $file_type;
+
+        $this->connection = 'database';
+
+        $this->queue = 'whatsapp';
+
+        $this->delay = 5;
     }
 
     /**
@@ -54,9 +72,11 @@ class SendWhatsAppMsg implements ShouldQueue
     public function handle()
     {
         if (!$this->file_content) { // Send normal message
+            Log::debug('\n\r SendingTextMessage to Contact contact_Jid from Job: ', [$this->Contact->whatsapp_id]);
             $this->rpiController->sendTextMessage($this->message, $this->Contact);
         }
         else {
+            Log::debug('\n\r SendingFileMessage to Contact contact_Jid from Job: ', [$this->Contact->whatsapp_id]);
             $this->rpiController->sendFileMessage($this->file_content, $this->file_name, $this->file_type, $this->message, $this->Contact);
         }
 
