07bfa6deda52b55c6106ec9ac304262e259c33ce
egbertocr <egberto.caballero@gmail.com>
2020-03-23 20:22:15 -0300

    aa

--- app/Http/Controllers/ExtendedContactController.php
+++ app/Http/Controllers/ExtendedContactController.php
@@ -111,6 +111,7 @@ class ExtendedContactController extends ContactController
             $lineWarn1 = array();
             $lineWarn2 = array();
             $lineWarn3 = array();
+            $lineWarn4 = array();
             
             //insert contacts in database
             $cntMessage1 = 0;
@@ -118,6 +119,8 @@ class ExtendedContactController extends ContactController
             $cntMessage3 = 0;
             $cntMessage4 = 0;
             $cntMessage5 = 0;
+            $cntMessage6 = 0;
+            $cntMessage7 = 0;
             $i=2;
             foreach($Contacts as $contact){
                 try{
@@ -126,6 +129,8 @@ class ExtendedContactController extends ContactController
                     $Contact = Contact::where('whatsapp_id' ,$whatsapp)
                             ->where('company_id', '=', $User->company_id)
                             ->first();
+                    
+                    if($Contact)$cntMessage7++;    
 
                     $Contact = $Contact ?? new Contact;
                     $latestAttendantContact = $Contact->latestAttendantContact()->first();
@@ -135,7 +140,11 @@ class ExtendedContactController extends ContactController
                     $Contact->origin = 3;
                     if (preg_match("/^[a-z A-Z0-9çÇáÁéÉíÍóÓúÚàÀèÈìÌòÒùÙãÃõÕâÂêÊôÔûÛñ\._-]{2,150}$/" , $contact['Nome'])) {
                         $Contact->first_name = trim($contact['Nome']);
+                    }else{
+                        $lineWarn4[$cntMessage6] = array("line" => "$i");
+                        $cntMessage6++;
                     }
+
                     if (isset($contact['Email']) && filter_var(trim($contact['Email']), FILTER_VALIDATE_EMAIL)) {
                         $Contact->email = trim($contact['Email']);
                     }
@@ -256,9 +265,17 @@ class ExtendedContactController extends ContactController
                 "lineError"  => $lineError
             );
 
+            $response["message6"] = array(
+                // "message" => " contato foi adicionado mas o nome do contato contem cracteres inválidos.",
+                "code" => "warning",
+                "lineWarn"  => $lineWarn4
+            );
+            $addingCnt = $cntMessage1-$cntMessage7;
             $response["statistics"] = array(
-                "successCnt" => "$cntMessage1",
+                "addingCnt" => "$addingCnt",
+                "updateCnt" => "$cntMessage7",
                 "warningCnt" => "$warningCnt",
+                "errorNameCnt" => "$cntMessage6",
                 "errorCnt" => "$cntMessage5",
             );
 
