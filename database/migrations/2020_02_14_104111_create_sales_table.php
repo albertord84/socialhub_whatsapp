9af29c3dd5a1a505bed822dbc36035ff109b6dd4
Your Name <you@example.com>
2020-04-20 19:51:31 -0300

    Auto message

--- database/migrations/2020_02_14_104111_create_sales_table.php
+++ database/migrations/2020_02_14_104111_create_sales_table.php
@@ -14,21 +14,21 @@ class CreateSalesTable extends Migration {
 	public function up(string $table_name = "sales")
 	{
 		try {
-			$this->down($table_name);
+			// $this->down($table_name);
+			Schema::connection('socialhub_mvp.sales')->create($table_name, function(Blueprint $table)
+			{
+				$table->string('id')->unique()->nullable(false);
+				$table->integer('contact_id')->nullable();
+				$table->integer('source')->nullable()->default(1);
+				$table->integer('sended')->nullable()->default(0);
+				$table->string('message', 1000)->nullable();
+				$table->string('json_data', 10000)->nullable();
+				$table->timestamps();
+				$table->softDeletes();
+			});
 		} catch (\Throwable $th) {
 			//throw $th;
 		}
-		Schema::connection('socialhub_mvp.sales')->create($table_name, function(Blueprint $table)
-		{
-			$table->string('id')->unique()->nullable(false);
-			$table->integer('contact_id')->nullable();
-			$table->integer('source')->nullable()->default(1);
-			$table->integer('sended')->nullable()->default(0);
-			$table->string('message', 1000)->nullable();
-			$table->string('json_data', 10000)->nullable();
-			$table->timestamps();
-			$table->softDeletes();
-		});
 	}
 
 
