e3b2d35f550e6cf2f3f88fde786188d016f3b87a
Your Name <you@example.com>
2020-04-22 20:25:07 -0300

    Tracking Correios Implemented + Tested + Visual

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -71,6 +71,8 @@ class TrackingController extends AppBaseController
         $page = (int) ($request->page ?? '0');
         $trackings = $this->trackingRepository->trackingByCompany($User->company_id, $page);
 
+        dd($trackings);
+
         return $trackings->toJson();
 
         // return view('trackings.index')
