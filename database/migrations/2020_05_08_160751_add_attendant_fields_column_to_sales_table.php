46ca6a20b301a1add131b8915fa67d6e15597803
Albert Reyes <alberto@artur.2020>
2020-05-11 16:06:03 -0300

    migration sales fields

--- database/migrations/2020_05_08_160751_add_attendant_fields_column_to_sales_table.php
+++ database/migrations/2020_05_08_160751_add_attendant_fields_column_to_sales_table.php
@@ -17,7 +17,7 @@ class AddAttendantFieldsColumnToSalesTable extends Migration
             $table->integer('has_attendant')->nullable()->default(0);
             $table->integer('attendant_id')->nullable();
             $table->integer('chat_id')->nullable();
-
+            $table->integer('status_id')->nullable();
         
         });
     }
