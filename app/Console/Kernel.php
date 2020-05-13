c2d07dffa304de8fa1ef43f5bd0f5564fbc77af9
Albert Reyes <alberto@artur.2020>
2020-05-13 16:43:44 -0300

    10 min

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('command:create_company_api_jobs')->cron('*/2 * * * *');
         $schedule->command('command:Create_company_bling_jobs')->cron('*/2 * * * *');
-        $schedule->command('command:Create_company_tracking_jobs')->cron('*/50 * * * *');
+        $schedule->command('command:Create_company_tracking_jobs')->cron('*/10 * * * *');
         $schedule->command('command:Get_sales_bling')->cron('0 */2 * * *');
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
